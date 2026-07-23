5abdc0bfa3efc5b292def5deefe6347aa93f279a
Julianne J. Cruz <juliannecruz14@gmail.com>
2026-07-24 06:07:02 +0800

    fix: update email templates directory path to use process.cwd()

--- server/src/services/emailService.js
+++ server/src/services/emailService.js
@@ -6,7 +6,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const templatesDir = path.join(__dirname, '../../../templates/emails');
+const templatesDir = path.join(process.cwd(), 'templates/emails');
 
 const TEMPLATE_DEFAULTS = {
   booking: {
